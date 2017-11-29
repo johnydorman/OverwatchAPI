23067c5eb3022bd31be7f4a3d454ec7e854ae946
johnydorman <Johndorman0@gmail.com>
2017-11-29 01:00:49 +0000

    Initial working build

---
+++ OverwatchAPI/DataObjects.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverwatchStats
+{
+    class Profile
+    {
+        public string icon { get; set; }
+        public string name { get; set; }
+        public string level { get; set; }
+        public string levelIcon { get; set; }
+        public string prestige { get; set; }
+        public string rating { get; set; }
+        public string ratingIcon { get; set; }
+        public string ratingName { get; set; }
+        public string gamesWon { get; set; }
+
+        public GameStats quickPlayStats { get; set; }
+        public GameStats competitiveStats { get; set; }
+    }
+
+    class GameStats
+    {
+        public int eliminationsAvg { get; set; }
+        public int damageDoneAvg { get; set; }
+        public int deathsAvg { get; set; }
+        public int finalBlowsAvg { get; set; }
+        public int healingDoneAvg { get; set; }
+        public int objectiveKillsAvg { get; set; }
+        public string objectiveTimeAvg { get; set; }
+        public int soloKillsAvg { get; set; }
+
+        public Awards awards { get; set; }
+        public Games games { get; set; }
+
+    }
+
+    class Awards
+    {
+        public int cards { get; set; }
+        public int medals { get; set; }
+        public int medalsBronze { get; set; }
+        public int medalsSilver { get; set; }
+        public int medalsGold { get; set; }
+    }
+
+    class Games
+    {
+        public int played { get; set; }
+        public int won { get; set; }
+    }
+}
---
+++ OverwatchAPI/DataRequest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//IO 
+using System.Net;
+using System.IO;
+
+//JSON
+using Newtonsoft.Json;
+
+namespace OverwatchStats
+{
+    delegate void Callback(DataRequest data);
+
+    class DataRequest
+    {
+        Callback callback;
+
+        public Profile profile { get; private set; }
+
+        public DataRequest(Callback cb)
+        {
+            callback = cb;
+            profile = new Profile();
+        }
+
+        public async Task<bool> Lookup(string name)
+        {
+            string url = "https://ow-api.com/v1/stats/pc/eu/" + name + "/profile";
+            string json = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    json = reader.ReadToEnd();
+                }
+            }
+            catch (WebException wException)
+            {
+                return false;
+            }
+
+            ParseJson(json);
+            callback(this);
+            return true;
+        }
+
+        private void ParseJson(string data)
+        {
+            profile = JsonConvert.DeserializeObject<Profile>(data);
+        }
+    }
+}
